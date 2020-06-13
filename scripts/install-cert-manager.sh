5ee55b224e539ac7ab2f2548be9e59c70eedbe9f
Javier Palomo <javier.palomo.almena@gmail.com>
2020-06-13 17:20:40 +0200

    Add steps to deploy the LetsEncrypt issuers

--- scripts/install-cert-manager.sh
+++ scripts/install-cert-manager.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
 
+TIMEOUT="60s"
+
+echo "# Installing cert-manager"
 kubectl apply \
     --validate=false \
     -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.yaml
+echo
 
-TIMEOUT="60s"
-echo "Will wait for ${TIMEOUT} for the cert-manager deployments before continuining"
-
-# Wait until the deployments are available before continuining
+echo "# Ensure the cert-manager deployments are ready before continuining. Will wait ${TIMEOUT}"
 kubectl wait \
     --timeout=${TIMEOUT} \
     --for=condition=available \
     deployment.apps \
     -l app.kubernetes.io/instance=cert-manager \
     -n cert-manager
+echo
+
+# Issuers
+echo "# Installing LetsEncrypt issuers"
+kubectl apply -f k8s/issuer/staging.yaml
+kubectl apply -f k8s/issuer/production.yaml
+echo
+
+echo "# Ensure the LetsEncrypt staging issuer is ready before continuining. Will wait ${TIMEOUT}"
+kubectl wait \
+    --timeout=${TIMEOUT} \
+    --for=condition=ready \
+    clusterissuer.cert-manager.io/letsencrypt-staging
+echo
+
+echo "# Ensure the LetsEncrypt production issuer is ready before continuining. Will wait ${TIMEOUT}"
+kubectl wait \
+    --timeout=${TIMEOUT} \
+    --for=condition=ready \
+    clusterissuer.cert-manager.io/letsencrypt-production
+echo
